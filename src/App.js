81b1ae7d2179418d4fa4daa97942acf0471e571a
ichido <ichido@live.jp>
2020-05-30 14:36:34 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Display from './Components/Display'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    function createData(itemA, itemB, itemC) {
+        return { itemA, itemB, itemC};
+    }
+    const data = [
+        createData(10, 20, 30)
+    ];
+    console.log('data = ', data);
+    // const data = [{ itemA: 10 }, { itemB: 20 }, { itemC: 30 }];
+    return (
+        <div>
+            <Display data={data} />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Components/Display.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles({
+    table: {
+        minWidth: 650,
+    },
+});
+  
+export default function Display(props) {
+    const classes = useStyles();
+
+    return (
+        <div>
+            <TableContainer component={Paper}>
+                <Table className={classes.table} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell align="right">項目A</TableCell>
+                            <TableCell align="right">項目B</TableCell>
+                            <TableCell align="right">項目C</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell align="right">{props.data[0].itemA}</TableCell>
+                            <TableCell align="right">{props.data[0].itemB}</TableCell>
+                            <TableCell align="right">{props.data[0].itemC}</TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </div>
+    );
+}
